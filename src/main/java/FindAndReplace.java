8a138728d11dfdfa29e1d980606ef2f98c57e9ed
Anna Lawley and Megan Fayer <mefayer@gmail.com>
2016-02-10 15:47:39 -0800

    Add front end

--- src/main/java/FindAndReplace.java
+++ src/main/java/FindAndReplace.java
@@ -1,8 +1,32 @@
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
 public class FindAndReplace {
   public static void main(String[] args) {
+  String layout = "templates/layout.vtl";
 
+  get("/", (request, response) -> {
+     Map<String, Object> model = new HashMap<String, Object>();
+     model.put("template", "templates/home.vtl");
+     return new ModelAndView(model, layout);
+   }, new VelocityTemplateEngine());
+
+   get("/results", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/results.vtl");
+
+      String input = request.queryParams("text");
+      String toFind = request.queryParams("toFind");
+      String toReplace = request.queryParams("toReplace");
+      String finished = findReplace(input, toFind, toReplace);
+
+      model.put("finished", finished);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
   public static String findReplace(String input, String toFind, String toReplace) {
     String[] phraseSplit = input.split(" ");
